08d7cb777b1dce311782be6105bba84c65095cb5
erezrokah <erezrokah@users.noreply.github.com>
2020-05-19 18:28:34 +0300

    chore: initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 const { Audit, RenderJson } = require('is-website-vulnerable');
+const puppeteer = require('puppeteer');
 
 module.exports = {
-  name: '@erezrokah/netlify-plugin-is-website-vulnerable',
-  // users will be tempted to use semver, but we really don't care
-  onPostDeploy: async ({ pluginConfig = {} } = {}) => {
-    const { site = process.env.SITE } = pluginConfig;
-
+  name: 'erezrokah/netlify-plugin-is-website-vulnerable',
+  onSuccess: async () => {
     try {
-      if (!site) {
-        throw new Error('Empty site');
+      const url = process.env.DEPLOY_URL;
+      if (!url) {
+        throw new Error('Empty url');
       }
+
+      const browserFetcher = puppeteer.createBrowserFetcher();
+      const revisions = await browserFetcher.localRevisions();
+      if (revisions.length <= 0) {
+        throw new Error('Could not found local browser');
+      }
+      const info = await browserFetcher.revisionInfo(revisions[0]);
+      process.env.CHROME_PATH = info.executablePath;
+
+      console.log('Scanning URL', url);
       const audit = new Audit();
-      const results = await audit.scanUrl(site);
+      const results = await audit.scanUrl(url);
       if (results.lhr.runtimeError) {
         throw new Error(results.lhr.runtimeError.message);
       }
@@ -21,13 +30,9 @@ module.exports = {
       console.log(JSON.stringify(json.vulnerabilities, null, 2));
 
       if (audit.hasVulnerabilities(results)) {
-        process.exit(2);
       }
-
-      process.exit(0);
     } catch (error) {
       console.error(`\nError: ${error.message}\n`);
-      process.exit(1);
     }
   },
 };
