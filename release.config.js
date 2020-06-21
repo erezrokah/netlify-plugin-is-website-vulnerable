49be7f4a78d42bf326b22740fda8f7840e648d54
erezrokah <erezrokah@users.noreply.github.com>
2020-06-21 17:09:24 +0300

    chore: improve release process

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    '@semantic-release/git',
+  ],
+};
