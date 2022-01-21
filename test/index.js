a821c24be1a17235324495509b2f07d11c80085f
erezrokah <erezrokah@users.noreply.github.com>
2022-01-21 11:16:50 +0100

    fix: update http-server, fix audit, add tests

---
+++ test/index.js
@@ -0,0 +1,21 @@
+const test = require('ava');
+const path = require('path');
+const netlifyBuild = require('@netlify/build');
+
+const NETLIFY_CONFIG = path.join(__dirname, '../netlify.toml');
+
+test('Netlify Build fail due to vulnerabilities', async (t) => {
+  const { success, logs } = await netlifyBuild({
+    config: NETLIFY_CONFIG,
+    buffer: true,
+  });
+
+  // Check that build failed as the example site has
+  t.false(success);
+
+  // Netlify Build output
+  const output = [logs.stdout.join('\n'), logs.stderr.join('\n')]
+    .filter(Boolean)
+    .join('\n\n');
+  t.snapshot(output);
+});
